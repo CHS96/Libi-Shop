7e3d7929b3b04e382259049d9e597334c60ed21d
CHS96 <sorjfkrh5078@gmail.com>
2021-08-03 12:24:35 +0900

    Repository 인터페이스, 메모리 제거

--- src/main/java/com/myservice/domain/item/ItemRepository.java
+++ src/main/java/com/myservice/domain/item/ItemRepository.java
@@ -1,21 +1,44 @@
 package com.myservice.domain.item;
 
 import com.myservice.domain.cart.Cart;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Repository
-public interface ItemRepository {
+@RequiredArgsConstructor
+@Primary
+public class ItemRepository {
 
-    Long save(Item item);
+    private final EntityManager em;
 
-    Optional<Item> findById(Long id);
+    public Long save(Item item) {
+        em.persist(item);
+        return item.getId();
+    }
 
-    List<Item> findAll();
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
 
-    void delete(Long itemId);
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
 
-    void saveItem(Cart cart);
+    public void delete(Long itemId) {
+        Item item = findById(itemId).get();
+        em.remove(item);
+    }
+
+    public void saveItem(Cart cart) {
+        em.persist(cart);
+    }
 }
--- src/main/java/com/myservice/domain/member/MemberRepository.java
+++ src/main/java/com/myservice/domain/member/MemberRepository.java
@@ -1,18 +1,44 @@
 package com.myservice.domain.member;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository {
+@RequiredArgsConstructor
+@Primary
+@Slf4j
+public class MemberRepository {
 
-    Long save(Member member);
+    private final EntityManager em;
 
-    Optional<Member> findById(Long id);
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
 
-    Optional<Member> findByLoginId(String loginId);
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
 
-    List<Member> findAll();
+    public Optional<Member> findByLoginId(String loginId) {
+        Member member = em.createQuery("select m from Member m where m.loginId = :loginId", Member.class)
+                .setParameter("loginId", loginId)
+                .getResultStream()
+                .findAny()
+                .orElse(null);
+
+        return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
 }
